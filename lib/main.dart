83ec4386824b8fa73d544c8f1b4a951120815d4e
MarcioQuimbundo <marcioquimbundo@hotmail.com>
2019-07-16 21:43:08 +0100

    snackbar added

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final _toDoController = TextEditingController();
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
   @override
   void initState() {
@@ -96,7 +98,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _buildItem(context, index) {
+  Widget _buildItem(BuildContext context, int index) {
     return Dismissible(
       key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
       background: Container(
@@ -128,6 +130,29 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved["title"]} removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
